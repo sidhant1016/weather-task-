f377c6ed47a2cc592d90565085ffdf66569c55c4
sidhant <gagranirocksidhanth@gmail.com>
2023-05-15 14:36:47 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import axios from 'axios';
+
 import './App.css';
 
 function App() {
+  const [city, setCity] = useState('');
+  const [weatherData, setWeatherData] = useState(null);
+
+  const handleChange = (e) => {
+    setCity(e.target.value);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.request(options);
+      setWeatherData(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  
+
+  const options = {
+    method: 'GET',
+    url: 'https://api.openweathermap.org/data/2.5/weather?q=vadodara&appid=2ee60f4df5cc617a4ee6be757ccdb93f',
+    params: {city},
+  headers: {
+    'X-RapidAPI-Key': '340ff2bcc0mshb1e32c10b6349d0p144914jsn6292109753c6',
+    'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+  }
+};
+   
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>WeatherReport</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={city} onChange={handleChange} placeholder="Enter city name" />
+        <button type="submit">Get Weather</button>
+      </form>
+      {weatherData && (
+        <div>
+          <h2>{weatherData.name}</h2>
+          
+        </div>
+      )}
     </div>
   );
 }
